8350a6240192679efd03cf6453381da02b31ebc3
Jillberth Estillore <ejillberth@gmail.com>
2015-06-25 00:52:40 +0800

    generate route sms url string

--- src/PhpRouteSms/RouteSms.php
+++ src/PhpRouteSms/RouteSms.php
@@ -11,8 +11,50 @@ namespace Jestillore\PhpRouteSms;
 
 class RouteSms {
 
+    private $packageName = 'php-route-sms';
+
+    private $server = '';
+    private $port = '';
+    private $username = '';
+    private $password = '';
+    private $dlr = '';
+    private $url = '';
+
+    private $type = '';
+    private $destination = '';
+    private $source = '';
+    private $message = '';
+
+    public function __construct() {
+        $this->server = $this->getConfig('server');
+        $this->port = $this->getConfig('port');
+        $this->username = $this->getConfig('username');
+        $this->password = $this->getConfig('password');
+        $this->dlr = $this->getConfig('dlr');
+        $this->url = $this->getConfig('url');
+    }
+
+    private function getConfig($config) {
+        return config($this->packageName . '.' . $config);
+    }
+
     public function test() {
-        return 'test route';
+        /*
+         * type
+         * destination
+         * source
+         * message
+         * */
+        return "";
+    }
+
+    public function sendSMS($config) {
+        $this->type = $config['type'];
+        $this->destination = urldecode(join(',', $config['destination']));
+        $this->source = urldecode($config['source']);
+        $this->message = urldecode($config['message']);
+
+        return "http://$this->server:$this->port/bulksms/bulksms?username=$this->username&password=$this->password&type=$this->type&dlr=$this->dlr&destination=$this->destination&source=$this->source&message=$this->message";
     }
 
 }
--- src/config/config.php
+++ src/config/config.php
@@ -7,6 +7,30 @@
     | Describe what it does. 
     */
 
-    //'key' => 'value',
+    'server' => '121.241.242.114',
+    'port' => '8080',
+
+    /*
+     * Username of the SMPP Account
+     * */
+    'username' => '',
+
+    /*
+     * Password of the SMPP Account
+     * */
+    'password' => '',
+
+    /*
+     * Indicates whether you want delivery report for this message
+     * */
+    'dlr' => '0', // 0 means no and 1 means yes
+
+    /*
+     * If sending a WAP Push message (type=4), this holds the link that you wish to send, for any
+     * other type of message, no value needs to be supplied for this field (if specified will be
+     * ignored) . Just like “message” field, this field should also be URL encoded with UTF-8
+     * character encoding (even for sending non-ASCII domain names).
+     * */
+    'url' => ''
 
 ];
